c5c132db9261031328bd9c5f9f28648df4e1861d
albanie <albanie@robots.ox.ac.uk>
2019-12-01 06:31:22 +0000

    cleanup links

--- setup_symlinks.sh
+++ setup_symlinks.sh
@@ -1,6 +1,8 @@
 # link to standard locations
-ln -s ~/.config/flake8 flake8
-ln -s ~/.config/yapf yapf
-ln -s ~/.pylintrc pylintrc
-ln -s ~/.inputrc inputrc
-ln -s ~/.vimrc vimrc
+ln -nfs "${PWD}/flake8" ~/.config/flake8
+ln -nfs "${PWD}/yapf" ~/.config/yapf
+ln -nfs "${PWD}/pylintrc" ~/.pylintrc
+ln -nfs "${PWD}/inputrc" ~/.inputrc
+ln -nfs "${PWD}/vimrc" ~/.vimrc
+ln -nfs "${PWD}/dircolors.ansi-dark" ~/.dircolors
+
